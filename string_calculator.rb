0de2b08f358eef059f36e11fa210c0e67f3e6965
parthibeyond <parthi@beyondfinity.com>
2024-05-13 16:16:31 +0530

    feat: enable add method to handle basic usecases

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
   def self.add(input)
+    return 0 if input.empty?
+    raise 'invalid input' if input.include?(",\n") || input.include?("\n,")
 
+    input.split(/,|\n/).map(&:to_i).sum
   end
 end
